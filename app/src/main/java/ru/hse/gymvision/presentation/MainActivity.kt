95edbad368c5951be3dd48cb1f5f30e55369126e
Мария Макаревич <maramakarevich@mail.ru>
2025-01-13 17:23:21 +0300

    Initial commit

---
+++ app/src/main/java/ru/hse/gymvision/presentation/MainActivity.kt
@@ -0,0 +1,47 @@
+package ru.hse.gymvision.presentation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ru.hse.gymvision.presentation.ui.theme.GymVisionTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GymVisionTheme {
+                Scaffold( modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Button(onClick = { }, modifier = Modifier.padding(innerPadding)) {
+                        Text("Click me!")
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GymVisionTheme {
+        Greeting("Android")
+    }
+}
